528fbfdbb3fbe427277d989fa55252f1cb89d0f1
Seungyeon0321 <71824405+Seungyeon0321@users.noreply.github.com>
2023-08-01 14:37:58 -0700

    First Commit

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import { Link } from "gatsby";
+import * as React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hi there! My name is Simon, and I'm the proud creator of this site!</p>
+    </Layout>
+  );
+};
+
+export const Head = () => <title>About Me</title>;
+
+export default AboutPage;
